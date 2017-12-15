9fc8ec60689c0b83e386fee5e65162ec9b809159
unknown <vishnuprabhut@gmail.com>
2017-12-14 23:20:35 -0500

    Train working!

--- AADT/AADT/Program.cs
+++ AADT/AADT/Program.cs
@@ -3,21 +3,24 @@ using LibSVMsharp.Helpers;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.IO;
 using System.Globalization;
 using System.Collections;
+using System.Text.RegularExpressions;
+using LibSVMsharp.Extensions;
 
 namespace AADT
 {
     class Program
     {
+        public static double totalSum { get; set; }
+        public Hashtable countHash = new Hashtable();
         private static string GetLineData(ref StreamReader reader,string line)
         {
             List<double> factorList=new List<double>();
             int sumVal = 0;
             string resultString = string.Empty;
+            int dayVolumeCount = 0;
             while (line != null)
             {
                 if (line.Contains("/2016") && !line.Contains("No Traffic Data is Available for Site"))
@@ -25,15 +28,18 @@ namespace AADT
                     DateTime date = DateTime.ParseExact(line, "d", CultureInfo.InvariantCulture);
                     string binaryMonth = Convert.ToString(date.Month, 2);
                     binaryMonth = binaryMonth.PadLeft(4, '0');
-                    resultString += "-" + binaryMonth;
+                    binaryMonth = Regex.Replace(binaryMonth, ".{1}", "$0-");
+                    resultString += binaryMonth;
                     string binaryDayOfWeek = Convert.ToString(Convert.ToInt32(date.DayOfWeek) + 1, 2);
                     binaryDayOfWeek = binaryDayOfWeek.PadLeft(3, '0');
-                    resultString += "-" + binaryDayOfWeek;
+                    binaryDayOfWeek = Regex.Replace(binaryDayOfWeek, ".{1}", "$0-");
+                    resultString += binaryDayOfWeek;
+                    dayVolumeCount = 0;
                 }
                 line = reader.ReadLine();
                 if(line == null)
                 {
-                    break;
+                    return "Skip!";
                 }
                 if (line.Contains(":") && !line.Contains("No Traffic Data is Available for Site"))
                 {
@@ -42,6 +48,10 @@ namespace AADT
                         line += line + " 0";
                     }
                     int numVal = (line.Split(' ')[1]=="N/A"? 0 : Convert.ToInt32(line.Split(' ')[1])) + (line.Split(' ')[4] == "N/A" ? 0 : Convert.ToInt32(line.Split(' ')[4]));
+                    if (numVal == 0)
+                    {
+                        return "Skip!";
+                    }
                     factorList.Add(numVal);
                     sumVal += numVal;
                 }
@@ -50,24 +60,25 @@ namespace AADT
                     break;
                 }
             }
+            dayVolumeCount = sumVal;
+            totalSum += sumVal;
             foreach (double i in factorList)
             {
                 double factor = Math.Round(i / sumVal,5);
-                resultString += "-" + factor.ToString();
+                resultString += factor.ToString()+"-";
             }
-            return resultString;
+            return resultString+"#"+dayVolumeCount.ToString();
         }
         private void ExtractData(string fileLocation)
         {            
             string line=string.Empty;
+            totalSum = 0;
             string currentATR = fileLocation.Replace(@"C:\Users\Vishnu\Downloads\ATR_Data_2016\","").Replace(".txt","");
-            if (currentATR == "0")
-            {
-                Console.WriteLine();
-            }
             StreamReader reader = new StreamReader(fileLocation);
             //TextWriter writer = new StreamWriter(@"C:\Users\Vishnu\Desktop\Result.txt");
             List<string> resultList = new List<string>();
+            Hashtable aadtVal = new Hashtable();
+            int count = 0;
             while (line != null)
             {
                 line = reader.ReadLine();
@@ -77,14 +88,29 @@ namespace AADT
                 }
                 if (line.Contains("/2016"))
                 {
-                    string resultString = currentATR+Program.GetLineData(ref reader, line);
-                    resultList.Add(resultString);
+                    string resultString = currentATR + "-" + Program.GetLineData(ref reader, line);
+                    if (resultString.Length > 27 && resultString!="Skip!")
+                    {
+                        resultList.Add(resultString);
+                        count++;
+                        aadtVal.Add(currentATR.ToString()+"->"+count.ToString(), resultString.Substring(resultString.IndexOf('#')+1));
+                        resultString = resultString.Remove(resultString.IndexOf('#'));
+                    }
                 }                
             }
-            TextWriter writer = File.AppendText(@"C:\Users\Vishnu\Desktop\Result.txt");
-            foreach(string s in resultList)
+            TextWriter writer = File.AppendText(@"C:\Users\Vishnu\Desktop\OP\Result.txt");
+            if (count > 180)
             {
-                writer.WriteLine(s);
+                int decr=1;
+                foreach (string s in resultList)
+                {
+                    double AADTFactor=Math.Round(Convert.ToInt32(aadtVal[currentATR.ToString() + "->" + (decr++).ToString()]) / (totalSum / count),3);
+                    writer.WriteLine(s.Remove(s.IndexOf('#'))+ AADTFactor.ToString());
+                }
+            }
+            else
+            {
+                Console.WriteLine(currentATR);
             }
             writer.Flush();
             reader.Close();
@@ -113,11 +139,84 @@ namespace AADT
             {
                 p.ExtractData(basePath+i.ToString()+".txt");
             }
-            
 
-            /*
-            SVMProblem problem = SVMProblemHelper.Load(@"C:\Users\Vishnu\Desktop\1.txt");
-            SVMProblem testProblem = SVMProblemHelper.Load(@"test_dataset_path.txt");
+            
+            List<int> inputATR = new List<int>();
+            List<int> trainATR = new List<int>();
+            List<int> testATR = new List<int>();
+            StreamReader streamReader = new StreamReader(@"C:\Users\Vishnu\Desktop\OP\Result.txt");
+            string line = streamReader.ReadLine();
+            while (line != null)
+            {
+                if (!inputATR.Contains(Convert.ToInt32(line.Substring(0,line.IndexOf('-')).ToString()))) 
+                {
+                    inputATR.Add(Convert.ToInt32(line.Substring(0, line.IndexOf('-')).ToString()));
+                }                
+                //Console.WriteLine(line);   
+                line = streamReader.ReadLine();
+            }
+            streamReader.Close();
+            int x = inputATR.Count();
+            int trainIndex = Convert.ToInt32(x * (0.80));
+            int testIndex = x - trainIndex;
+            StreamWriter trainWriter = new StreamWriter(@"C:\Users\Vishnu\Desktop\OP\Train.txt");
+            StreamWriter testWriter = new StreamWriter(@"C:\Users\Vishnu\Desktop\OP\Test.txt");
+            foreach (int i in Enumerable.Range(0, trainIndex))
+            {
+                trainATR.Add(inputATR[i]);
+            }
+            foreach (int i in Enumerable.Range(trainIndex, x - trainIndex))
+            {
+                testATR.Add(inputATR[i]);
+            }
+            streamReader = new StreamReader(@"C:\Users\Vishnu\Desktop\OP\Result.txt");
+            line = streamReader.ReadLine();
+            while (line != null)
+            {
+                string atr = line.Substring(0, line.IndexOf('-'));
+                int colonCount = 1;
+                if (trainATR.Contains(Convert.ToInt32(line.Substring(0, line.IndexOf('-')).ToString())))
+                {                                       
+                    string temp = String.Empty;
+                    line = line.Remove(0, line.IndexOf('-')+1);
+                    temp = atr;
+                    string[] tempArr = line.Split('-');
+                    foreach(string s in tempArr)
+                    {
+                       temp += " " + colonCount++.ToString() + ":" + s;                        
+                    }
+                    trainWriter.WriteLine(temp);
+                }
+                if (testATR.Contains(Convert.ToInt32(line.Substring(0, line.IndexOf('-')).ToString())))
+                {
+                    string temp = String.Empty;
+                    line = line.Remove(0, line.IndexOf('-') + 1);
+                    temp = atr;
+                    string[] tempArr = line.Split('-');
+                    foreach (string s in tempArr)
+                    {
+                        temp += " " + colonCount++.ToString() + ":" + s;
+                    }
+                    testWriter.WriteLine(temp);
+                }
+                line = streamReader.ReadLine();
+            }
+            streamReader.Close();
+            trainWriter.Flush();
+            trainWriter.Close();
+            testWriter.Flush();
+            testWriter.Close();
+
+            int[] xtrain = Enumerable.Range(1, 31).ToArray();
+            int[] xtest = Enumerable.Range(1, 31).ToArray();
+            int[] ytrain = Enumerable.Range(32, 1).ToArray();
+            int[] ytest = Enumerable.Range(32, 1).ToArray();
+
+            SVMProblem trainData = SVMProblemHelper.Load(@"C:\Users\Vishnu\Desktop\OP\Train.txt");
+            SVMProblem testData = SVMProblemHelper.Load(@"C:\Users\Vishnu\Desktop\OP\Test.txt");
+
+            trainData = trainData.Normalize(SVMNormType.L2);
+            testData = testData.Normalize(SVMNormType.L2);
 
             SVMParameter parameter = new SVMParameter();
             parameter.Type = SVMType.C_SVC;
@@ -125,14 +224,58 @@ namespace AADT
             parameter.C = 1;
             parameter.Gamma = 1;
 
-            SVMModel model = SVM.Train(problem, parameter);
 
-            double[] target = new double[testProblem.Length];
-            for (int i = 0; i < testProblem.Length; i++)
-                target[i] = SVM.Predict(model, testProblem.X[i]);
+            double[] crossValidationResults; // output labels
+            int nFold = 4;
+            trainData.CrossValidation(parameter, nFold, out crossValidationResults);
 
-            double accuracy = SVMHelper.EvaluateClassificationProblem(testProblem, target);
+            double crossValidationAccuracy = trainData.EvaluateClassificationProblem(crossValidationResults);
+
+            
+
+            SVMModel model = SVM.Train(trainData, parameter);
+            SVM.SaveModel(model, @"C:\Users\Vishnu\Desktop\OP\AADT_model.txt");
+
+            double[] testResults = testData.Predict(model);
+
+            double[] target = new double[testData.Length];
+            for (int i = 0; i < testData.Length; i++)
+                target[i] = SVM.Predict(model, testData.X[i]);
+
+            double accuracy = SVMHelper.EvaluateClassificationProblem(testData, target);
+
+            Console.WriteLine("\n\nCross validation accuracy: " + crossValidationAccuracy);
+            Console.WriteLine("Accuracy - " + accuracy);
+
+            /*int[,] confusionMatrix;
+            try
+            {
+                double testAccuracy = testData.EvaluateClassificationProblem(testResults, model.Labels, out confusionMatrix);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Key not found!!!!!!!");
+            }
+
+            
+            Console.WriteLine("\nTest accuracy: " + testAccuracy);
+            Console.WriteLine("\nConfusion matrix:\n");
+
+            Console.Write(String.Format("{0,6}", ""));
+            for (int i = 0; i < model.Labels.Length; i++)
+                Console.Write(String.Format("{0,5}", "(" + model.Labels[i] + ")"));
+            Console.WriteLine();
+            for (int i = 0; i < confusionMatrix.GetLength(0); i++)
+            {
+                Console.Write(String.Format("{0,5}", "(" + model.Labels[i] + ")"));
+                for (int j = 0; j < confusionMatrix.GetLength(1); j++)
+                    Console.Write(String.Format("{0,5}", confusionMatrix[i, j]));
+                Console.WriteLine();
+            }
             */
+
+            
+            Console.ReadLine();
         }
     }
 }
